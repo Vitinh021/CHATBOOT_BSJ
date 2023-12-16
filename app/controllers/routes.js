34e7ae2bf5d27484f8a105a712d59eb1b66bf4c9
Edson Costa de Siqueira Neto <edsonsiqueiraneto@gmail.com>
2023-12-16 18:20:24 -0300

    Foi feito: atualização do timeoutAtendimento. Criação de um primeiro menu antes de exibir as datas das extrações. Criação de alguns outros status. Para fazer: Mensagens de encerramento do ciclo, testar os valores de tempo de duração dos timeouts e executar mais testes no geral.

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -14,53 +14,53 @@ const utils = require('./utils.js');
 const app = express();
 
 
-const fs = require('fs');
-const wppconnect = require('@wppconnect-team/wppconnect');
 
-wppconnect
-  .create({
-    session: 'sessionName',
-    catchQR: (base64Qr, asciiQR) => {
-      console.log(asciiQR); // Optional to log the QR in the terminal
-      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-        response = {};
-
-      if (matches.length !== 3) {
-        return new Error('Invalid input string');
-      }
-      response.type = matches[1];
-      response.data = new Buffer.from(matches[2], 'base64');
-
-      var imageBuffer = response;
-      require('fs').writeFile('out.png', imageBuffer['data'], 'binary', function (err) {
-          if (err != null) {
-            console.log(err);
-          } else {
-                // Ler o arquivo recém-criado e enviar como resposta
-                fs.readFile('out.png', function (error, data) {
-                    if (error) {
-                        console.error(error);
-                        res.status(500).send('Erro ao ler o arquivo');
-                    } else {
-                        res.writeHead(200, {
-                            'Content-Type': 'image/png'
-                        });
-                        res.end(data); // Enviar o conteúdo do arquivo como resposta
-                    }
-                });
-            }
-        }
-      );
-    },
-    logQR: false,
-  })
-  .then((client) => utils.start(client))
-  .catch((error) => console.log(error));
 
 
 //LEMBRAR DE COLOCAR DO const fs = require('fs'); ATE .catch((error) => console.log(error)); DENTRO DA ROTA
-//app.post('/gerar', (req, res) => {
-//
-//});
+app.post('/gerar', (req, res) => {
+  const fs = require('fs');
+  const wppconnect = require('@wppconnect-team/wppconnect');
+  
+  wppconnect
+    .create({
+      session: 'sessionName',
+      catchQR: (base64Qr, asciiQR) => {
+        console.log(asciiQR); // Optional to log the QR in the terminal
+        var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+          response = {};
+  
+        if (matches.length !== 3) {
+          return new Error('Invalid input string');
+        }
+        response.type = matches[1];
+        response.data = new Buffer.from(matches[2], 'base64');
+  
+        var imageBuffer = response;
+        require('fs').writeFile('out.png', imageBuffer['data'], 'binary', function (err) {
+            if (err != null) {
+              console.log(err);
+            } else {
+                  // Ler o arquivo recém-criado e enviar como resposta
+                  fs.readFile('out.png', function (error, data) {
+                      if (error) {
+                          console.error(error);
+                          res.status(500).send('Erro ao ler o arquivo');
+                      } else {
+                          res.writeHead(200, {
+                              'Content-Type': 'image/png'
+                          });
+                          res.end(data); // Enviar o conteúdo do arquivo como resposta
+                      }
+                  });
+              }
+          }
+        );
+      },
+      logQR: false,
+    })
+    .then((client) => utils.start(client))
+    .catch((error) => console.log(error));
+});
 
 module.exports = app;
