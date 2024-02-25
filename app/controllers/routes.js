e628b2347d3fbcd8680318d51ae6d9728e04c792
NikollasBeltrao <64939921+NikollasBeltrao@users.noreply.github.com>
2024-02-25 16:32:58 -0300

    teste

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -22,15 +22,10 @@ app.get('/run', async (req, res) => {
   try {
     const client = await wppconnect.create({
       session: "sessionName",
-      headless: 'new',
-      devtools: false,
-      useChrome: false,
-      debug: false,
-      logQR: true,
-      puppeteerOptions: puppeteerOptions,
-      disableWelcome: true,
-      updatesLog: false,
+      headless: "new", // Headles  s chrome
+      debug: true,
       autoClose: false,
+      disableWelcome: true,
       catchQR: (base64Qr, asciiQR) => {
         console.log("QR code recebido");
         var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
