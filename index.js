7dad2960d296d3c2abd9e523f48f9e95eda60f08
Nikollas Beltrão <beltraonikollas@gmail.com>
2024-03-01 14:24:39 -0300

    teste

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 //const server = require('./app/config/server.js');
 
 const express = require('express');
@@ -6,14 +5,8 @@ const app = express();
 
 const routes = require('./app/controllers/routes.js')
 
-let url = 'https://gestaobsj.com.br/Server/status.php?getByPhone=true&phone=8';
-console.log('deu adasdas')
+app.use('/', routes)
 
-axios.get(url, { timeout: 5000000 }) // Definindo o timeout como 50 segundos
-  .then(response => {
-    console.log('deu certo')
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+app.listen(3000, () => {
+    console.log(`rodando na porta 3000`)
+})
