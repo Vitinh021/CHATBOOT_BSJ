0246ef14c8830ed969d5c2514a7e1aa0fabe0525
Nikollas Beltrão <beltraonikollas@gmail.com>
2024-04-26 11:02:39 -0300

    a

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -17,7 +17,7 @@ const puppeteerOptions = {
   defaultViewport: null, // Permite configurar o tamanho da janela do navegador
   args: ['--no-sandbox', '--disable-setuid-sandbox'], // Argumentos adicionais para o Chrome/Chromium
   //executablePath: '/usr/bin/chromium-browser' // Especifique o caminho do Chrome aqui
-  executablePath: '/root/.cache/chrome-headless-shell/linux-124.0.6367.91/chrome-headless-shell-linux64/chrome-headless-shell' // Especifique o caminho do Chrome aqui
+  executablePath: '/root/CHATBOOT_BSJ/chrome-headless-shell/linux-120.0.6098.0/chrome-headless-shell-linux64/chrome-headless-shell' // Especifique o caminho do Chrome aqui
 
 };
 // Inicia o cliente wppconnect quando o servidor Node.js é iniciado
